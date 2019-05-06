6f4afd7d35848ac53f0e0766c9f8b776e8257737
bcid <bcid@uci.edu>
2019-05-05 22:39:04 -0700

    Twitter pt 2

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -19,6 +19,12 @@ class HomeTableViewController: UITableViewController {
         loadTweet()
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
     }
     
     @objc func loadTweet(){
@@ -35,7 +41,7 @@ class HomeTableViewController: UITableViewController {
             self.tableView.reloadData()
             self.myRefreshControl.endRefreshing()
         }, failure: { (Error) in
-            print("Could not retrieve tweets")
+            print("Could not retrieve tweets \(Error)")
             
         })
     }
@@ -53,7 +59,7 @@ class HomeTableViewController: UITableViewController {
                 }
                 self.tableView.reloadData()
         }, failure: { (Error) in
-            print("Could not retrieve tweets")
+            print("Could not retrieve tweets \(Error)")
         })
     }
     
@@ -84,6 +90,9 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweed(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,8 +13,64 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    var retweeted:Bool = false
 
+    @IBAction func rewteet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweed(true)
+        }, failure: { (Error) in
+            print("Error in retweeting")
+        })
+        
+        
+    }
+    
+    @IBAction func favorite(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+            self.setFavorite(false)
+        }, failure: { (error) in
+            print("Unfavorite did not succeed: \(error)")
+        })
+            
+        }
+    }
+    
+    func setRetweed(_ isRetweeted: Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "Image-2"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "Image-3"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+        
+    }
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named:"Image-1"), for: UIControl.State.normal)
+        }
+        else{
+            favButton.setImage(UIImage(named:"Image"), for: UIControl.State.normal)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
